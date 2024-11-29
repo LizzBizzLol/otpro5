e57015ee67bc17bca8d451e327af1dfcfa981c74
LizzBizzLol <150434567+LizzBizzLol@users.noreply.github.com>
2024-11-30 00:54:20 +0500

    Update test_routes.py

--- test_routes.py
+++ test_routes.py
@@ -1,1 +1,9 @@
+from fastapi.testclient import TestClient
+from main import app
 
+client = TestClient(app)
+
+def test_get_all_nodes():
+    response = client.get("/api/nodes")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
