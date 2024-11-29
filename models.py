805efe4cc8cdb1eae4a7ef5ba9b4f0327b3ac6f5
LizzBizzLol <150434567+LizzBizzLol@users.noreply.github.com>
2024-11-30 00:38:17 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,9 @@
+from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom
 
+class Node(StructuredNode):
+    id = StringProperty(unique_index=True, required=True)
+    label = StringProperty(required=True)
+    relationships = RelationshipTo("Node", "CONNECTED_TO")
+
+class Relationship(StructuredNode):
+    type = StringProperty(required=True)
